d11334d5bbf500a76699b8e45aecc56c7890e24b
Luana Matias Terense <luana.r.matias@gmail.com>
2021-11-24 22:33:21 -0300

    Primeiro commit

---
+++ codecept.conf.js
@@ -0,0 +1,40 @@
+const { setHeadlessWhen } = require("@codeceptjs/configure");
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: "./*_test.js",
+  output: "./output",
+  helpers: {
+    REST: {
+      endpoint: "https://petstore.swagger.io/v2",
+      onRequest: (request) => {
+        request.headers.accept = "application/json";
+      },
+    },
+    ChaiWrapper: {
+      require: "codeceptjs-chai",
+    },
+  },
+  include: {
+    I: "./steps_file.js",
+  },
+  bootstrap: null,
+  mocha: {},
+  name: "codecept-js-api",
+  translation: "pt-BR",
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true,
+    },
+    tryTo: {
+      enabled: true,
+    },
+    screenshotOnFail: {
+      enabled: true,
+    },
+  },
+};
